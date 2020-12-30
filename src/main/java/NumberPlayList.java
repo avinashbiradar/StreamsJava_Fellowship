a6f41b10269aa6c26ae633ac1e34dd25d46c1195
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-30 09:30:30 +0530

    Added:Transforming each to double and store the result

--- src/main/java/NumberPlayList.java
+++ src/main/java/NumberPlayList.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class NumberPlayList {
     public static void main(String[] args) {
@@ -13,5 +15,10 @@ public class NumberPlayList {
         myNumberList.stream().forEach(n -> {
             System.out.println("METHOD1:Stream forEach Value::" + n);
         });
+        //Method-2 - process stream , apply operation ,and then store result
+        Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
+              List<Double>streamList=myNumberList.stream().map(toDoubleFunction).
+                collect(Collectors.toList());
+        System.out.println("METHOD2:Printing double list " + streamList);
     }
 }
