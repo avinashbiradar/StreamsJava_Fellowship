a1c5a1bba6d550db2e83a4748887d1be085a1d6e
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-30 10:39:04 +0530

    Added:Peak and show the first even number in the number stream

--- src/main/java/NumberPlayList.java
+++ src/main/java/NumberPlayList.java
@@ -12,19 +12,31 @@ public class NumberPlayList {
         for (int i = 0; i < 5; i++) {
             myNumberList.add(i);
         }
+
         //Method-1 processing the stream
         myNumberList.stream().forEach(n -> {
             System.out.println("METHOD1:Stream forEach Value::" + n);
         });
+
         //predicate even or odd
         Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
         myNumberList.forEach(n -> {
             System.out.println("method: forEach value of " + n + " check for even: " + isEvenFunction.test(n));
         });
+
         //Method-2 - process stream , apply operation ,and then store result
         Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
-              List<Double>streamList=myNumberList.stream().filter(isEvenFunction).map(toDoubleFunction).
-                collect(Collectors.toList());
+              List<Double>streamList=myNumberList.stream()
+                                      .filter(isEvenFunction)
+                                      .map(toDoubleFunction)
+                                      .collect(Collectors.toList());
         System.out.println("METHOD2:Printing double list " + streamList);
+
+        //method3 - to find the next peek of even number
+        Integer first = myNumberList.stream().
+                         filter(isEvenFunction)
+                        .peek(n-> System.out.println("peak even number "+n))
+                        .findFirst().orElse(null);
+        System.out.println("first even "+first);
     }
 }
