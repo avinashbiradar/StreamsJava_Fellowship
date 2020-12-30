c4b5ad2f7e4f10a97c5120590d28a916c0e22b93
avinashbiradar <avinashbiradar179@gmail.com>
2020-12-30 09:50:29 +0530

    Added:Filter the even number from stream and store the result

--- src/main/java/NumberPlayList.java
+++ src/main/java/NumberPlayList.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class NumberPlayList {
@@ -15,9 +16,14 @@ public class NumberPlayList {
         myNumberList.stream().forEach(n -> {
             System.out.println("METHOD1:Stream forEach Value::" + n);
         });
+        //predicate even or odd
+        Predicate<Integer> isEvenFunction = n -> n > 0 && n % 2 == 0;
+        myNumberList.forEach(n -> {
+            System.out.println("method: forEach value of " + n + " check for even: " + isEvenFunction.test(n));
+        });
         //Method-2 - process stream , apply operation ,and then store result
         Function<Integer, Double> toDoubleFunction = Integer::doubleValue;
-              List<Double>streamList=myNumberList.stream().map(toDoubleFunction).
+              List<Double>streamList=myNumberList.stream().filter(isEvenFunction).map(toDoubleFunction).
                 collect(Collectors.toList());
         System.out.println("METHOD2:Printing double list " + streamList);
     }
